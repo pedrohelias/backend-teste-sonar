f6560efa2a3f2ddc6a6cc26221c5927287a20022
Matheus Soares <matheus.1503@hotmail.com>
2024-11-13 23:19:02 -0300

    feat(teachers): Add error handling in api-gateway
    Co-authored-by: Mateus Caltabiano <mateus.frauzino@gmail.com>
    Co-authored-by: João Victor <Correia.jv16@gmail.com>

--- http-api-gateway/src/teachers/teacher.controller.ts
+++ http-api-gateway/src/teachers/teacher.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateTeacherDto } from './dtos/CreateTeacher.dto';
+import { lastValueFrom } from 'rxjs';
 
 @Controller('teachers')
 export class TeacherController {
   constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
   @Post()
-  createTeacher(@Body() createteacherDto: CreateTeacherDto) {
-    this.natsClient.emit('createTeacher', createteacherDto);
+  async createTeacher(@Body() createteacherDto: CreateTeacherDto) {
+    try {
+      const response = await lastValueFrom(
+        this.natsClient.send('createTeacher', createteacherDto)
+      );
+      return response;
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get()
