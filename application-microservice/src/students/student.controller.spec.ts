d480e643ab2091001f753d691499cb5b6d821c13
Pedro Haick <pehaick@proton.me>
2024-11-16 20:35:46 -0300

    Basic student CRUD testing

---
+++ application-microservice/src/students/student.controller.spec.ts
@@ -0,0 +1,103 @@
+import { Test } from '@nestjs/testing';
+import { CreateStudentDto } from './dtos/CreateStudent.dto';
+import { StudentMicroserviceController } from './student.controller';
+import { StudentsModule } from './student.module';
+import { StudentsService } from './student.service';
+
+describe('StudentsController', () => {
+  let studentsController: StudentMicroserviceController;
+  let studentsService: StudentsService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [StudentsModule],
+    }).compile();
+
+    studentsService = module.get(StudentsService);
+    studentsController = module.get(StudentMicroserviceController);
+  });
+
+  afterEach(() => jest.clearAllMocks());
+
+  describe('getAll', () => {
+    it('Should return an array of students', async () => {
+      expect(await studentsController.getAll()).toBeInstanceOf(Array<Object>);
+    });
+  });
+
+  describe('get', () => {
+    it('Should return a student', async () => {
+      const response = await studentsController.create({
+        name: 'Student',
+        categorie: 'PARCIAL',
+        class: 'BERCARIO',
+        turn: 'MATUTINO',
+      });
+
+      const studentId = response.data.id;
+
+      expect(await studentsController.get(studentId)).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('create', () => {
+    it('Should create a student', async () => {
+      const createStudent: CreateStudentDto = {
+        name: 'Student',
+        categorie: 'PARCIAL',
+        class: 'BERCARIO',
+        turn: 'MATUTINO',
+      };
+
+      const { message, data } = await studentsController.create(createStudent);
+
+      expect(message).toBe('Student created successfully');
+      expect(data).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('update', () => {
+    it('Should update a student', async () => {
+      const response = await studentsController.create({
+        name: 'Student',
+        categorie: 'PARCIAL',
+        class: 'BERCARIO',
+        turn: 'MATUTINO',
+      });
+
+      const studentId = response.data.id;
+
+      const updatedStudent: CreateStudentDto = {
+        name: 'Student',
+        categorie: 'INTEGRAL',
+        class: 'BERCARIO',
+        turn: 'MATUTINO',
+      };
+
+      const { message, data } = await studentsController.update({
+        data: updatedStudent,
+        studentId,
+      });
+
+      expect(message).toBe('Student updated successfully');
+      expect(data).toMatchObject(updatedStudent);
+    });
+  });
+
+  describe('disable', () => {
+    it('should disable a student', async () => {
+      const response = await studentsController.create({
+        name: 'Student',
+        categorie: 'PARCIAL',
+        class: 'BERCARIO',
+        turn: 'MATUTINO',
+      });
+
+      const studentId = response.data.id;
+
+      const { message } = await studentsController.disable(studentId);
+
+      expect(message).toBe('Student deleted successfully');
+    });
+  });
+});
