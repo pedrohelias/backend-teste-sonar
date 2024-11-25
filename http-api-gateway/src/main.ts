b0baa848840c004962278ce311365253cd2ab1dc
Pedro Haick <pehaick@proton.me>
2024-11-24 23:59:46 -0300

    Swagger set up on API gateway

--- http-api-gateway/src/main.ts
+++ http-api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,10 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  const config = new DocumentBuilder().setTitle('Infantio').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(PORT, () => console.log(`Running on PORT ${PORT}`));
 }
